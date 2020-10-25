219dd41add09d79f23219f67208d4c9c998c5502
Daniel Lopez <lopezd6@gmail.com>
2020-10-25 20:12:08 +0100

    Updates

---
+++ precache-manifest.afc6b3bc268de0a6b27fa7543c10c5a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90d645113a756516a8aa77b4ee0af45",
+    "url": "/election-visualizer/index.html"
+  },
+  {
+    "revision": "7268676357f4799083f4",
+    "url": "/election-visualizer/static/css/main.557e9523.chunk.css"
+  },
+  {
+    "revision": "b2e19f61847eceee2b9b",
+    "url": "/election-visualizer/static/js/2.2a07a2d3.chunk.js"
+  },
+  {
+    "revision": "cbf7ab8ad9b210a8e3f9021a24e25fd5",
+    "url": "/election-visualizer/static/js/2.2a07a2d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7268676357f4799083f4",
+    "url": "/election-visualizer/static/js/main.9396f840.chunk.js"
+  },
+  {
+    "revision": "21e08cf441f6541b97bc",
+    "url": "/election-visualizer/static/js/runtime-main.926f4712.js"
+  }
+]);
