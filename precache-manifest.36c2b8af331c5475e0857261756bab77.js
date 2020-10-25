b506a156114a853e35d7cf790b6b94d3b405248a
Daniel Lopez <lopezd6@gmail.com>
2020-10-25 18:39:06 +0100

    Updates

---
+++ precache-manifest.36c2b8af331c5475e0857261756bab77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec7f5edfb573100161b74ff94e575b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f656eb91d1d2b13980f8",
+    "url": "/static/css/main.c050aeda.chunk.css"
+  },
+  {
+    "revision": "6b045221997bd7861e66",
+    "url": "/static/js/2.e44280c8.chunk.js"
+  },
+  {
+    "revision": "cbf7ab8ad9b210a8e3f9021a24e25fd5",
+    "url": "/static/js/2.e44280c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f656eb91d1d2b13980f8",
+    "url": "/static/js/main.d536eb25.chunk.js"
+  },
+  {
+    "revision": "c0e8b641d37f9999ce6c",
+    "url": "/static/js/runtime-main.40898a0b.js"
+  }
+]);
