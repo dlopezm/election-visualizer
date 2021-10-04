9d6bdaf3694ec9842e8ea79f1fc019e0a143c376
Dani Lopez <daniell@salsitasoft.com>
2021-10-04 12:16:35 +0200

    Add Fall 2021 results

--- src/elections.ts
+++ src/elections.ts
@@ -1,5 +1,10 @@
 export const ELECTIONS = [
     {
+        fileName: 'Fall_2021.csv',
+        title: 'Fall 2021',
+        system: 'score',
+    },
+    {
         fileName: 'Spring_2021.csv',
         title: 'Spring 2021',
         system: 'stv',
